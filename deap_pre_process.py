54ed4452e8e7cd24ff01cf5d98a0e1cb93991fd5
ShivamShrirao <shivamshrirao@gmail.com>
2019-03-08 03:48:19 +0530

    val,aro separate; shuffled dataset into seconds; changed little of network

--- deap_pre_process.py
+++ deap_pre_process.py
@@ -75,10 +75,10 @@ def pre_process_range(directory=DATASET_DIR,start_range=1,end_range=33):
             file_name='s'+str(0)+str(i)
         else:
             file_name='s'+str(i)
-        print("[+] Reading:",file_name)
+        print("[+] Reading subject:",file_name)
         temp=pickle.load(open(directory+file_name+'.dat','rb'),encoding='latin1')
 
-        temp['labels'] = (temp['labels'][:,:2]>5)*1
+        labels = (temp['labels'][:,:2]>5)*1
         print("[*] Processing:",file_name)
         
         N = (sample_rate*3)
@@ -98,8 +98,19 @@ def pre_process_range(directory=DATASET_DIR,start_range=1,end_range=33):
             eeg_2D.append(norm_2D_dataset(eeg))
 
         print('\r100 %')
-        eeg_1D=np.array(eeg_1D)
-        eeg_2D=np.array(eeg_2D)
+        eeg_1D=np.array(eeg_1D).reshape(40*60, 128, 32)     #(seconds, frequency, ...)
+        eeg_2D=np.array(eeg_2D).reshape(40*60, 128, 9, 9)   #(seconds, frequency, ...)
+        labels=np.repeat(labels,60,axis=0)                  #(2400,2)
+
+        #Randomizing per second data
+        print("[*] Shuffling seconds.")
+        rand_ind = np.array(range(40*60))
+        np.random.shuffle(rand_ind)
+        eeg_1D=eeg_1D[rand_ind]
+        eeg_2D=eeg_2D[rand_ind]
+        labels=labels[rand_ind]
+
+        labels=labels.transpose()
 
         print("[+] Writing processed:",file_name)
         with open(PROCESSED_DIR+file_name+"_cnn.pkl","wb") as f:
@@ -107,19 +118,19 @@ def pre_process_range(directory=DATASET_DIR,start_range=1,end_range=33):
         with open(PROCESSED_DIR+file_name+"_rnn.pkl","wb") as f:
             pickle.dump(eeg_1D,f,protocol=4)
         with open(PROCESSED_DIR+file_name+"_labels.pkl","wb") as f:
-            pickle.dump(temp['labels'],f)
+            pickle.dump(labels,f)
         print("[*] Done.\n")
 
 def multi_pre_process_all():
     MUL=32//THREADS
     Pool().starmap(pre_process_range,[(DATASET_DIR,i*MUL+1,(i+1)*MUL+1) for i in range(THREADS)])
 
-def get_subject_data(subject_no,media_no):
+def get_subject_data(subject_no):
     '''
         Gets the data for a preticular subject
         ARGS:
         subject_no: ID of subject
-        media_no: The meida for which the data was shown
+        media_no: The media for which the data was shown
         RETURN VALS:
         eeg data 1D (normalized)
         eeg data 2D (normalized)
@@ -127,12 +138,10 @@ def get_subject_data(subject_no,media_no):
         arousal value
     '''
     # For the subject selection (1-32)
-    media_no=list(media_no)
     if subject_no<10:
         file_name='s'+str(0)+str(subject_no)
     else:
         file_name='s'+str(subject_no)
-    print("[+] Reading subject:",file_name,"Media:",media_no)
     #Importing the file
     file_dir = PROCESSED_DIR+file_name
     #Fetching the required data
@@ -141,21 +150,12 @@ def get_subject_data(subject_no,media_no):
     else:
         pass
 
-    eeg_1D_normalized = pickle.load(open(file_dir+"_rnn.pkl", 'rb'))
-    eeg_2D_normalized = pickle.load(open(file_dir+"_cnn.pkl", 'rb'))
+    eeg_1D = pickle.load(open(file_dir+"_rnn.pkl", 'rb'))   #(40*60, 128, 32)
+    eeg_2D = pickle.load(open(file_dir+"_cnn.pkl", 'rb'))   #(40*60, 128, 9, 9)
     #Fetching valance and arousal
-    labels=pickle.load(open(file_dir+"_labels.pkl", 'rb'))
-    eeg_1D_list=[]
-    eeg_2D_list=[]
-    valance=[]
-    arousal=[]
-    for i in media_no:
-        eeg_1D_list.append(eeg_1D_normalized[i])
-        eeg_2D_list.append(eeg_2D_normalized[i])
-        valance.append(labels[i][0])
-        arousal.append(labels[i][1])
+    labels=pickle.load(open(file_dir+"_labels.pkl", 'rb'))  #(2, 2400)
     #Returning the dataset
-    return eeg_1D_list,eeg_2D_list,valance,arousal
+    return eeg_1D,eeg_2D,labels[0],labels[1]
 
 if __name__ == '__main__':
     try:
